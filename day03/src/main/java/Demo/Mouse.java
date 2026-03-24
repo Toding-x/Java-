abfe026b6438d824dc95258ecdf010f738ccf7ab
toding-x <toding0215@163.com>
2026-03-24 16:27:14 +0800

    Java学习

--- day03/src/main/java/Demo/Mouse.java
+++ day03/src/main/java/Demo/Mouse.java
@@ -2,6 +2,14 @@ package Demo;
 
 public class Mouse extends Animal{
     public Mouse(String name,int id){
-        super("老鼠",21);
+        super(name, id);
+
+    }
+    public static void main(String []args){
+        Mouse mouse = new Mouse("老鼠",22);
+        mouse.eat();
+        mouse.sleep();
+        mouse.introduction();
+
     }
 }
--- day03/src/main/java/Demo/Penguin.java
+++ day03/src/main/java/Demo/Penguin.java
@@ -2,6 +2,6 @@ package Demo;
 
 public class Penguin extends Animal{
     public Penguin(String name, int id){
-        super("企鹅",12);
+        super(name, id);
     }
 }
