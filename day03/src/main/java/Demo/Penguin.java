160a595aa4f49eccf0a2c10987dbd7422cfeb96e
toding-x <toding0215@163.com>
2026-03-24 16:28:20 +0800

    Java学习

--- day03/src/main/java/Demo/Penguin.java
+++ day03/src/main/java/Demo/Penguin.java
@@ -4,4 +4,11 @@ public class Penguin extends Animal{
     public Penguin(String name, int id){
         super(name, id);
     }
+    public static void main(String []args){
+        Penguin penguin = new Penguin("企鹅",12);
+        penguin.eat();
+        penguin.sleep();
+        penguin.introduction();
+
+    }
 }
